45977e1b85f2b8e5687cef6688e0dfc3f35823f5
HasibTB12 <hotafghangirl121@gmail.com>
2020-11-05 18:13:23 -0500

    Updated Login

--- index.js
+++ index.js
@@ -1,44 +1,11 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
-import routes from "./routes.js";
-import Header from "./Header";
-import "./styles.css";
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./components/App"
+import "bootstrap/dist/css/bootstrap.min.css"
 
-import * as firebase from "firebase";
-import firebaseConfig from "./firebase.config";
-
-firebase.initializeApp(firebaseConfig);
-
-
-export const AuthContext = React.createContext(null);
-
-function App() {
-  const [isLoggedIn, setLoggedIn] = useState(false);
-
-
-  return (
-    <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
-      Is logged in? {JSON.stringify(isLoggedIn)}
-      <div className="App">
-        <Router>
-          <Header />
-
-          <Switch>
-            {routes.map(route => (
-              <Route
-                key={route.path}
-                path={route.path}
-                exact={route.exact}
-                component={route.main}
-              />
-            ))}
-          </Switch>
-        </Router>
-      </div>
-    </AuthContext.Provider>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
