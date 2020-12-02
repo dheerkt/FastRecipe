f7559382b2ce96af13ad2fb5bfec85bb2b823b5d
Dheer Toprani <Dheerkt@users.noreply.github.com>
2020-12-02 18:14:33 +0400

    Added sorting results, and a few bug fixes

--- src/screens/RecipeScreen.js
+++ src/screens/RecipeScreen.js
@@ -1,32 +1,57 @@
-import { database } from "firebase";
 import React from "react";
 import { ScrollView, View } from "react-native";
-import {Text, Image, Rating , Button} from "react-native-elements"
+import {Text, Image, Badge} from "react-native-elements";
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 export default function RecipeScreen(props) {
     const result = (props.navigation.getParam('data'));
     return (
     <ScrollView>
-        <View>
-
-        <Image
+        <View style={{backgroundColor: "#000", marginBottom: 10}}>
+            <Image
             source={{ uri: result.avatar }}
-            style={{ width: "100%", height: 400, backgroundColor: "black"}}
-        >        
-        </Image>
-
-        <Button style={{zIndex: "999"}} title="Back" onPress={() => props.navigation.navigate('Recipes')}/>
-
-        <Text h3 style={{position: "absolute", bottom: 50, left: 25, color: "#FFF"}}>{result.name}</Text>
+            style={{ width: "100%", height: 400, opacity: 0.65}}
+            />
+            <Text 
+            style={{position: "absolute", top: 25, left: 20, color: "#FFF"}}
+            onPress={() => props.navigation.navigate('Recipes')}
+            >
+                <FontAwesome
+                size={26}
+                name="chevron-left"
+                color="#FFF"/>
+            </Text>
+            <Text h3 style={{position: "absolute", bottom: 25, left: 25, color: "#FFF"}}>{result.name}</Text>
         </View>
-        <View style={{marginHorizontal: 25, marginVertical: 10}}>
+
+        <View>
+            <Badge 
+            status="primary" 
+            badgeStyle={{ 
+                height: 35, paddingRight: 10, paddingLeft: 10, marginHorizontal: 5,
+                borderWidth: 1, borderColor: "#e0e0e0", backgroundColor: "#fff",
+            }}
+            value={
+                <Text>
+                    <FontAwesome
+                    name="coffee"
+                    size={14}
+                    color="#43484d"
+                    />
+                    &nbsp;
+                    Lol
+                </Text>
+            }/>
+        </View>
+
+        <View style={{marginHorizontal: 25}}>
             <View style={{marginVertical: 15}}>
                 <Text h4>Ingredients</Text>
-                <Text>{result.ingredients}</Text>
+                <Text style={{fontSize: 16, lineHeight: 25}}>{result.ingredients}</Text>
             </View>
             <View>
                 <Text h4>Method</Text>
-                <Text>{result.instructions}</Text>
+                <Text style={{fontSize: 16, lineHeight: 25}}>{result.instructions}</Text>
             </View>
         </View>
 
--- src/screens/RecipesScreen.js
+++ src/screens/RecipesScreen.js
@@ -4,24 +4,68 @@ import "../components/firebase-config.js";
 import firebase from "firebase";
 import RecipeCard from '../components/RecipeCard'
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import { Menu, Provider } from 'react-native-paper';
+import { Button } from 'react-native-elements'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function RecipesScreen({navigation}) {
-    const [data, setData] = useState('');
+    const [visible, setVisible] = React.useState(false);
+    const openMenu = () => setVisible(true);
+    const closeMenu = () => setVisible(false);
     
+    const [data, setData] = useState('');
+    const [order, setOrder] = useState('timestamp');
+    
+    // dbref
+    const dbref = firebase.database().ref()
+    // Get data from firebase
     useEffect(() => {
-        async function getData() {
-            const snapshot = await firebase.database().ref("recipes").once("value")
-            setData(snapshot.val())
-        }
-        getData();
-    }, [])
+        function getData() {
+            let arr = []
+            let query = dbref.child("recipes").orderByChild(order)
 
+            query.on("value", function(snapshot) {
+                snapshot.forEach(function(child) {
+                    arr.push(child.val())
+                })
+
+                if(order == "timestamp" || order == "rating") {
+                    arr.reverse()
+                }
+                setData(arr)
+            })
+        }
+        getData()
+    }, [order])
+    
     return(
+        <Provider>
         <ScrollView>
             <SafeAreaView>
+                <View style={{marginHorizontal: 15, marginTop: 15}}>
+                    <Menu
+                    visible={visible}
+                    onDismiss={closeMenu}
+                    anchor={<Button
+                    icon={
+                        <Icon
+                            style={{marginRight: "5%"}}
+                            name="chevron-down"
+                            size={17.5}
+                            color="white"
+                        />
+                    }
+                    onPress={openMenu}
+                    style={{ width: "30%" }}
+                    title="Sort By" />}>
+                        <Menu.Item onPress={() => {setOrder('timestamp')}} title="Newest" />
+                        <Menu.Item onPress={() => {setOrder('time')}} title="Fastest Cook Time" />
+                        <Menu.Item onPress={() => {setOrder('rating')}} title="Best Rated" />
+                    </Menu>
+                </View>
                 <RecipeCard result={(Object.entries(data))} navigation={navigation}></RecipeCard>
             </SafeAreaView>
-        </ScrollView>
+        </ScrollView></Provider>
     )
+
 }
